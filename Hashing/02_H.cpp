b776118ae274e52bf9eadff5da15139e70b13b88
Vishnu <sahalvishnuhari237@gmail.com>
2026-01-23 01:45:48 +0530

    Hashing Basics

--- Hashing/02_H.cpp
+++ Hashing/02_H.cpp
@@ -1,34 +1,35 @@
-#include <iostream>
-#include <bits/stdc++.h>
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main(){
     int n;
-    cout << "Enter Number of Elements in Array ";
+    cout << "Enter Number of elements in an Array : ";
     cin >> n;
-
     int arr[n];
-
+    cout<<"Enter elements in Array : \n";
     for (int i = 0; i < n; i++)
     {
-        cout << "Enter element at index [" << i << "]";
+        cout << "Array Index " << i << ": ";
         cin >> arr[i];
     }
-    //Precompute
+
+    // Precompute
     int hash[13] = {0};
     for (int i = 0; i < n; i++)
     {
         hash[arr[i]] += 1;
     }
     
+    cout << "Enter how many Numbers for which you have to check : ";
     int q;
     cin >> q;
     while(q--){
-        int number;
-        cin >> number;
-        //Fetch
-        cout << hash[number] << endl;
+        int num;
+        cout << "Number " << q <<" : ";
+        cin >> num;
+
+        cout << hash[num]<<endl;
     }
 
     return 0;
