76aca8ea366c44c6946f84fc5301d817b1fda2c6
Vishnu <sahalvishnuhari237@gmail.com>
2026-01-25 17:27:15 +0530

    Hashing

--- Hashing/03_H.cpp
+++ Hashing/03_H.cpp
@@ -1,8 +1,24 @@
+/*
+Array size can be maximum of 10pow(6) inside main
+and 10pow(7) outside main
+Character size can be maximum of 10pow(7) inside main
+and 10pow(8) outside main
+
+
+
+*/
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    
+    string s = "hello";
+    char target = 'l';
+    int cnt = 0;
+    for(char c : s){
+        cnt += (c == target);
+    }
+    cout << cnt;
     return 0;
 }
---
+++ Hashing/04_H.cpp
@@ -0,0 +1,11 @@
+/*
+Character Hashing
+*/
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
