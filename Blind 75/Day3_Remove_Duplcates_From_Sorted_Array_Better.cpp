2d1b0e73e155107557513c6f3663599c261a9048
Vishnu <sahalvishnuhari237@gmail.com>
2026-02-11 19:04:17 +0530

    DAy3

--- Blind 75/Day3_Remove_Duplcates_From_Sorted_Array_Better.cpp
+++ Blind 75/Day3_Remove_Duplcates_From_Sorted_Array_Better.cpp
@@ -1,3 +1,30 @@
 /*
-
+Optimal used 2 pointers
 */
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void remove_duplicates(vector<int> &arr, int n)
+{
+    int i = 0;
+    for (int j = 1; j < n; j++)
+    {
+        if (arr[i] != arr[j])
+        {
+            arr[i + 1] = arr[j];
+            i++;
+        }
+    }
+    for (int k = 0; k <= i; k++)
+    {
+        cout << arr[k] << " ";
+    }
+}
+int main()
+{
+    vector<int> arr = {1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6};
+    int n = arr.size();
+
+    remove_duplicates(arr, n);
+    return 0;
+}
